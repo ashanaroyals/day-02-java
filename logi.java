8e5948a2df2d63b0f55a8d25817c3a132492204e
ashanaroyals <kgashalatha3@gmail.com>
2024-12-03 14:36:28 +0530

    Add files via upload

---
+++ logi.java
@@ -0,0 +1,12 @@
+
+public class logi
+{
+    public static void main(String args[])
+    {
+    
+        System.out.println(true&&true);
+        System.out.println(false||false);
+        System.out.println(!(true));
+        
+    }
+}
