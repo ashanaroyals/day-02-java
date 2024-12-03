9c9f2fac11142f4db12eb44a505ce3894367cb31
ashanaroyals <kgashalatha3@gmail.com>
2024-12-03 10:07:02 +0530

    Add files via upload

---
+++ g.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class g
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        string a=sc.next();
+        int b=sc.nextInt();
+        string c=string.format("the name of the person is %s and the age is %d",a,b);
+
+    }
+}
