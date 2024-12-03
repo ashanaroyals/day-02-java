48565db6312d1825537b67d299156ee8981b2d0e
ashanaroyals <kgashalatha3@gmail.com>
2024-12-03 10:54:38 +0530

    Add files via upload

---
+++ assi.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class assi
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println(a+=98);
+        System.out.println(a-=9);
+        System.out.println(a*=75);
+    }
+}
